ba8dafb44999a0e3b5405e9d53be3b8cbc545bf6
Blake Foster <rblakefoster314@gmail.com>
2022-11-21 00:00:32 -0500

    Tooltips

--- src/components/input/VertexControl.js
+++ src/components/input/VertexControl.js
@@ -7,16 +7,18 @@ import { CORNER, CENTER } from "../../model/VertexConfig";
 
 class VertexControl extends React.Component {
 
-  renderButton(vertexLocation, checked, onClick) {
+  renderButton(vertexLocation, checked, onClick, tooltip) {
     return (
       <div className="vertexLocationButton">
-        <FormControlLabel
-          value={vertexLocation.name}
-          control={<Switch/>}
-          label={vertexLocation.name}
-          checked={checked}
-          onClick={onClick}
-        />
+        <Tooltip title={tooltip}>
+          <FormControlLabel
+            value={vertexLocation.name}
+            control={<Switch/>}
+            label={vertexLocation.name}
+            checked={checked}
+            onClick={onClick}
+          />
+        </Tooltip>
       </div>
     );
   }
@@ -28,14 +30,19 @@ class VertexControl extends React.Component {
           this.renderButton(
             CORNER,
             this.props.vertexConfig.enableCorner,
-            () => this.props.setVertexConfig(this.props.vertexConfig.toggleCorner())
+            () => this.props.setVertexConfig(this.props.vertexConfig.toggleCorner()),
+            "Vertices will be at the corners of the sides. Best for constructing triangles with hinge plates."
           )
         }
         {
           this.renderButton(
             CENTER,
             this.props.vertexConfig.enableCenter,
-            () => this.props.setVertexConfig(this.props.vertexConfig.toggleCenter())
+            () => this.props.setVertexConfig(this.props.vertexConfig.toggleCenter()),
+            "Vertices will be located in the center of the final stud on each side. Best for constructing triangles " +
+            "with single stud connections as the hinges. Tip: in this configuration, all 3 sides extend 1.5 bricks " +
+            "beyond the vertex on each end, which adds 1 stud to the length of each side. If you have a hard upper " +
+            "limit on side length you may want to subtract 1 from each max to compensate."
           )
         }
       </InputGroup>
